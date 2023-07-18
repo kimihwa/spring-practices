6425e8e63a9b588042a410e4868ebfc45cd9bbb7
김이화 <@DESKTOP-OAHFS92>
2023-07-18 14:01:25 +0900

    emaillist03

---
+++ emaillist03/src/main/java/com/bitacademy/emaillist/controller/EmaillistController.java
@@ -0,0 +1,27 @@
+package com.bitacademy.emaillist.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.bitacademy.emaillist.repository.EmaillistRepository;
+import com.bitacademy.emaillist.vo.EmaillistVo;
+
+@Controller
+public class EmaillistController {
+	@Autowired
+	private EmaillistRepository emaillistRepository;
+	
+	@ResponseBody
+	@RequestMapping("/")
+	public String index(Model model) {
+		List<EmaillistVo> list = emaillistRepository.findAll();
+		model.addAttribute("list", list);
+		
+		return"/WEB-INF/views/index.jsp";
+	}
+}
